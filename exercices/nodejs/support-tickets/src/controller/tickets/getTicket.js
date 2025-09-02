ce173a32570368809d5a6d550cfea81c0ffdfa53
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-09-02 19:47:45 -0300

    add get params

--- exercices/nodejs/support-tickets/src/controller/tickets/getTicket.js
+++ exercices/nodejs/support-tickets/src/controller/tickets/getTicket.js
@@ -1,8 +1,24 @@
+import url from "url";
+
 export function getTicket(req, res, database) {
+    const parsedUrl = url.parse(req.url, true);
+
     if (database) {
-        return res.writeHead(201).end(JSON.stringify(database.read("tickets")))
-    }
-    else {
+        if (parsedUrl.query.status) {
+            if (parsedUrl.query.status !== "open" && parsedUrl.query.status !== "closed") {
+                return res.writeHead(400).end("Status inválido");
+            }
+
+            return res.writeHead(201).end(
+                JSON.stringify(
+                    database.read("tickets").find((ticket) => {
+                        return ticket.status === parsedUrl.query.status;
+                    })
+                )
+            );
+        }
+        return res.writeHead(201).end(JSON.stringify(database.read("tickets")));
+    } else {
         return es.writeHead(400).end("Sem banco de dados");
     }
 }
--- exercices/nodejs/support-tickets/src/middleware/routeHandler.js
+++ exercices/nodejs/support-tickets/src/middleware/routeHandler.js
@@ -1,11 +1,14 @@
+import url from "url";
 import { routes } from "../routes/index.js";
 import { Database } from "../database/database.js";
-const database = new Database()
+const database = new Database();
 
 export function routeHandler(req, res) {
+    const parsedUrl = url.parse(req.url, true);
+
     const route = routes.find((route) => {
-        return route.method === req.method && route.path === req.url;
-    })
+        return route.method === req.method && route.path === parsedUrl.pathname;
+    });
 
     if (route) {
         return route.controller(req, res, database);
